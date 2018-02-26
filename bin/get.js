1e7f2ca746cd7f8e6bd410dcea9baa0ec22b8b12
Luis A. Echeverria <pixelsnob@gmail.com>
2018-02-25 18:24:28 -0800

    Return value from runner injected script

--- bin/get.js
+++ bin/get.js
@@ -1,6 +1,15 @@
 
-const config         = require('../config');
+const config         = require('../config'),
+      { promisify }  = require('util'),
+      fs             = require('fs'),
+      readFileAsync  = promisify(fs.readFile);
+
 const getAll = require('../lib/get');
 const runner = require('../lib/runner');
 
-runner(getAll, config);
+(async function () {
+    const res = await runner(getAll, config);
+    //console.log(res);
+    // save to csv
+})();
+
