2e21990ca504f1aad46ee8a2c63d560eae698034
Luis A. Echeverria <pixelsnob@gmail.com>
2018-02-25 22:37:03 -0800

    Alerts export

--- bin/get.js
+++ bin/get.js
@@ -2,14 +2,19 @@
 const config         = require('../config'),
       { promisify }  = require('util'),
       fs             = require('fs'),
-      readFileAsync  = promisify(fs.readFile);
+      writeFileAsync = promisify(fs.writeFile);
 
 const getAll = require('../lib/get');
 const runner = require('../lib/runner');
 
+const filename = './data/alerts-exported.csv';
+
 (async function () {
     const res = await runner(getAll, config);
-    //console.log(res);
-    // save to csv
+    if (Array.isArray(res) && res.length) {
+        console.log(res.length + ' alerts found');
+        await writeFileAsync(filename, res.join("\n"));
+        console.log(`File ${filename} written successfully`);
+    }
 })();
 
