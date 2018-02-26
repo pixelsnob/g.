4d2c5a2d3731121b8e915ca1e195c771e4b3bf54
Luis A. Echeverria <pixelsnob@gmail.com>
2018-02-25 16:10:33 -0800

    Added get script

--- bin/get.js
+++ bin/get.js
@@ -1,8 +1,6 @@
 
-const config   = require('../config');
-
+const config         = require('../config');
 const getAll = require('../lib/get');
 const runner = require('../lib/runner');
 
 runner(getAll, config);
-
